459e51adf8b7e63d711aa55e06658859d13bd9c8
Certuche <yan-carlos.certuche@capgemini.com>
2026-03-03 16:45:15 -0500

    Se crea linea base del proyecto

---
+++ src/main/java/com/univalle/parkingmanagementservice/ParkingManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.univalle.parkingmanagementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingManagementServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/univalle/parkingmanagementservice/ParkingManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.univalle.parkingmanagementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
